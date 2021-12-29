386541fda686ada32750b5c65970e251b6a42681
yoooomi030 <yoooomi030@gmail.com>
2021-12-29 18:51:02 +0900

    숙제 커밋

--- homework/Qu04.py
+++ homework/Qu04.py
@@ -1,4 +1,9 @@
 star="*"
 blank=" "
-for i in range(5):
-    for j in range(5):
+for i in range(1,6):
+    for j in range(1,6):
+        if i+j<=6:
+            print(star, end='')
+        else:
+            print(blank, end='')
+    print()
